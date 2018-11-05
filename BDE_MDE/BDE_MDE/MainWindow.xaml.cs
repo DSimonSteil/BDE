ffd3767e2cb5d8d3e2ac8c5dd0e2cb01a65c2e98
dsimon <dsimon@steil.de>
2018-11-05 16:44:55 +0100

    Erste Schritte

---
+++ BDE_MDE/BDE_MDE/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BDE_MDE
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBoxResult result = MessageBox.Show("Danke",
+                                          "Confirmation",
+                                          MessageBoxButton.YesNo,
+                                          MessageBoxImage.Question);
+            }
+            catch (Exception exc)
+            {
+                Feedback(exc);
+            }
+        }
+
+
+        private void Feedback(Exception exc)
+        {
+            tbx_feedback.Visibility = Visibility.Visible;
+            tbx_feedback.Text += exc.GetType().ToString() + @" @ " + new System.Diagnostics.StackTrace().GetFrame(1).GetMethod().Name 
+                + System.Environment.NewLine + exc.Message + System.Environment.NewLine + System.Environment.NewLine;
+        }
+    }
+}
