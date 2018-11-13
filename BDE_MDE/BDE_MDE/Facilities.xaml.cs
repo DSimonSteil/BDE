e527833e5c76ff3a76d13aa52e26dc8695a1fcf0
dsimon <dsimon@steil.de>
2018-11-13 11:38:56 +0100

    Kondirator, TMA

--- BDE_MDE/BDE_MDE/Facilities.xaml.cs
+++ BDE_MDE/BDE_MDE/Facilities.xaml.cs
@@ -22,14 +22,18 @@ namespace BDE_MDE
     public partial class Facilities : Page
     {
         #region Variables
-        string str_configFilePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\bdeConfig.xml";
-        string str_companyLocation = String.Empty;
-        string str_faciltyImgPath = String.Empty;
+        private string str_configFilePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\bdeConfig.xml";
+        private string str_branch = String.Empty;
+        private string str_faciltyImgPath = String.Empty;
+        private string str_values = String.Empty;
+        private string[] stra_coords;
         #endregion
+
         public Facilities(string str_actualFacility)
         {
             InitializeComponent();
             RenderFacility(str_actualFacility);
+            CreateBoxes(str_actualFacility);
         }
 
         public void RenderFacility(string str_actualFacility)
@@ -52,13 +56,11 @@ namespace BDE_MDE
         {
             try
             {
-                XmlDocument xml_configFile = new XmlDocument();
-                //string str_configFilePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\bdeConfig.xml";
+                XmlDocument xml_configFile = new XmlDocument();                
                 xml_configFile.Load(str_configFilePath);
 
-                str_companyLocation = xml_configFile.SelectSingleNode(@"BDE.Configuration/general/Niederlassung").Attributes[@"value"].Value;
-                str_faciltyImgPath = xml_configFile.SelectSingleNode(@"BDE.Configuration/" + str_companyLocation + @"/platz_luftbild/" + str_actualFacility + @"/imagePath").Attributes[@"value"].Value;
-                //XmlNodeList xnList = xml_configFile.SelectNodes(@"BDE.Configuration/" + str_companyLocation + @"/platz_luftbild");                
+                str_branch = xml_configFile.SelectSingleNode(@"BDE.Configuration/General/Branch").Attributes[@"value"].Value;
+                str_faciltyImgPath = xml_configFile.SelectSingleNode(@"BDE.Configuration/" + str_branch + @"/Aerial_image/" + str_actualFacility + @"/imagePath").Attributes[@"value"].Value;                
             }
             catch (Exception exc)
             {
@@ -66,6 +68,65 @@ namespace BDE_MDE
             }
         }
 
+        public void CreateBoxes(string str_actualFacility)
+        {
+            try
+            {
+                XmlDocument xml_configFile = new XmlDocument();
+                xml_configFile.Load(str_configFilePath);                
+
+                XmlNodeList xnList = xml_configFile.SelectNodes(@"BDE.Configuration/" + str_branch + "/Facilities/" + str_actualFacility);
+
+                SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+                mySolidColorBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFB0D99D"));
+
+                foreach (XmlNode xn1 in xnList)
+                {
+                    foreach (XmlNode xn2 in xn1.ChildNodes)
+                    {                        
+                        foreach (XmlNode xn3 in xn2.ChildNodes)
+                        {
+                            str_values = xn3.Attributes[@"coords"].Value;
+                            stra_coords = str_values.Split(';');
+
+                            Button btn = new Button()
+                            {
+                                Name = xn3.Name,
+                                Content = xn3.Name.Substring(1),
+                                Background = mySolidColorBrush,
+                                Margin = new Thickness(Convert.ToDouble(stra_coords[0]), Convert.ToDouble(stra_coords[1]), 0, 0),
+                                FontWeight = FontWeights.Bold,
+                                FontSize = 35,
+                                Width = Convert.ToDouble(stra_coords[2]),
+                                Height = Convert.ToDouble(stra_coords[3]),
+                            };
+                            btn.Click += new RoutedEventHandler(btn_button_Click);
+                            FacilityGrid.Children.Add(btn);                                                                               
+                        }                                                
+                    }                    
+                }
+            }
+            catch (Exception exc)
+            {
+                Feedback(exc);
+            }
+        }
+
+        private void btn_button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Dem Developer zum Gruße!");
+                //Button btn = sender as Button;
+
+                //Facilities fac = new Facilities(btn.Content.ToString());
+                //this.NavigationService.Navigate(fac);
+            }
+            catch (Exception exc)
+            {
+                Feedback(exc);
+            }
+        }
 
         private void Feedback(Exception exc)
         {
--- BDE_MDE/BDE_MDE/ViewArea.xaml.cs
+++ BDE_MDE/BDE_MDE/ViewArea.xaml.cs
@@ -21,10 +21,16 @@ namespace BDE_MDE
     /// </summary>
     public partial class ViewArea : Page
     {
+        #region Variables
+        private string str_configFilePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\bdeConfig.xml";
+        private string str_branch = String.Empty;
+        private string str_values = String.Empty;
+        private string[] stra_coords;
+        #endregion
         public ViewArea()
         {
             InitializeComponent();
-            CreateButtons();
+            CreateFaciliteButtons();
         }
 
         private void btn_button_Click(object sender, RoutedEventArgs e)
@@ -38,22 +44,19 @@ namespace BDE_MDE
             }
             catch (Exception exc)
             {
-                MainWindow mw = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-                mw.Feedback(exc);
+                Feedback(exc);
             }
         }
 
-        private void CreateButtons()
+        private void CreateFaciliteButtons()
         {
             try
             {
-                XmlDocument xml_configFile = new XmlDocument();                
-                string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\bdeConfig.xml";
-                string str_values;
-                string[] stra_coords;
+                XmlDocument xml_configFile = new XmlDocument();                                                
+                xml_configFile.Load(str_configFilePath);
+                str_branch = xml_configFile.SelectSingleNode(@"BDE.Configuration/General/Branch").Attributes[@"value"].Value;
 
-                xml_configFile.Load(path);
-                XmlNodeList xnList = xml_configFile.SelectNodes(@"BDE.Configuration/Trier/platz_luftbild");
+                XmlNodeList xnList = xml_configFile.SelectNodes(@"BDE.Configuration/" + str_branch + "/Aerial_image");
 
                 SolidColorBrush mySolidColorBrush = new SolidColorBrush();
                 mySolidColorBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFB0D99D"));
@@ -71,26 +74,30 @@ namespace BDE_MDE
                             BorderThickness = new Thickness(2),
                             Name = xn2.Name,
                             Content = xn2.Name,
-                            Background = System.Windows.Media.Brushes.Transparent,                            
+                            Background = System.Windows.Media.Brushes.Transparent,
                             Margin = new Thickness(Convert.ToDouble(stra_coords[0]), Convert.ToDouble(stra_coords[1]), 0, 0),
                             Foreground = mySolidColorBrush,
                             FontWeight = FontWeights.Bold,
                             FontSize = 25,
                             Width = Convert.ToDouble(stra_coords[2]),
-                            Height = Convert.ToDouble(stra_coords[3]),                            
-                        };                                                                          
+                            Height = Convert.ToDouble(stra_coords[3]),
+                        };
                         AreaGrid.Children.Add(btn);
 
-                        btn.Click += new RoutedEventHandler(btn_button_Click);                        
-                    }                    
+                        btn.Click += new RoutedEventHandler(btn_button_Click);
+                    }
                 }
             }
             catch (Exception exc)
             {
-                MessageBox.Show(exc.GetType().ToString() + @" @ " + new System.Diagnostics.StackTrace().GetFrame(1).GetMethod().Name
-                + System.Environment.NewLine + exc.Message + System.Environment.NewLine + System.Environment.NewLine);
-            }            
+                Feedback(exc);
+            }
         }
-        
+
+        private void Feedback(Exception exc)
+        {
+            MessageBox.Show(exc.GetType().ToString() + @" @ " + new System.Diagnostics.StackTrace().GetFrame(1).GetMethod().Name
+                + System.Environment.NewLine + exc.Message + System.Environment.NewLine + System.Environment.NewLine);
+        }
     }
 }
